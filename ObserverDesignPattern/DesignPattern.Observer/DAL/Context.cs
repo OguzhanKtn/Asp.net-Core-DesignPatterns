3aa9b5aa3c5272fa01cd47e2e18d4a1a316ef152
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2024-06-13 23:16:25 +0300

    ObserverDesignPattern

---
+++ ObserverDesignPattern/DesignPattern.Observer/DAL/Context.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DesignPattern.Observer.DAL
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+    }
+}
