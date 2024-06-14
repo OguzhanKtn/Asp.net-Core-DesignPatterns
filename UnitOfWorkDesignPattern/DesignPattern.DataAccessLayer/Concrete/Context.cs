ba8dfa11b123330b6dcfbdfff1149674f83dad22
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-06-14 17:56:12 +0300

    UnitOfWork

---
+++ UnitOfWorkDesignPattern/DesignPattern.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options) 
+        { 
+
+        }
+       
+    }
+}
